dfbf1d86b515db9bcbcd28f5b4ac4e52cdbe3138
aluna30 <132224699+aluna30@users.noreply.github.com>
2025-05-01 20:07:18 -0400

    Delete SceneKeys.cs

--- SceneKeys.cs
+++
@@ -1,5 +0,0 @@
-public static class SceneKeys
-{
-    public static readonly string SCENE_NAME_MAIN_MENU = "Main_Menu";
-    public static readonly string SCENE_NAME_PUZZLE_LEVEL = "Puzzle_Level";
-}
