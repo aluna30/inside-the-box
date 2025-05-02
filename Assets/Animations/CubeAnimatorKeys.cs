d9bba4c8c6c6f5f51a775fb907b27f6a8d71535c
aluna30 <132224699+aluna30@users.noreply.github.com>
2025-05-01 20:15:18 -0400

    Add files via upload

---
+++ Assets/Animations/CubeAnimatorKeys.cs
@@ -0,0 +1,7 @@
+public static class CubeAnimatorKeys
+{
+    public static readonly string PARAM_MOVE_FORWARD = "moveForward";
+    public static readonly string PARAM_MOVE_BACKWARD = "moveBackward";
+    public static readonly string PARAM_MOVE_RIGHT = "moveRight";
+    public static readonly string PARAM_MOVE_LEFT = "moveLeft";
+}
